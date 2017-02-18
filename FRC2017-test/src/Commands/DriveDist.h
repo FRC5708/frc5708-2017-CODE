65b5e6b9164e91a8e07da6c9932a8b320ac201bd
synctax <benwyngaard@gmail.com>
2017-02-18 14:52:03 -0500

    need to commit for wierd reasons, will revert

---
+++ FRC2017-test/src/Commands/DriveDist.h
@@ -0,0 +1,20 @@
+#ifndef DriveDist_H
+#define DriveDist_H
+
+#include "CommandBase.h"
+
+/**
+ * The main DriveDist command to pickup and deliver the soda to the box.
+ */
+class DriveDist: public CommandBase {
+public:
+	DriveDist(double distance);
+	double Distance;
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+};
+
+#endif  // DriveDist_H
